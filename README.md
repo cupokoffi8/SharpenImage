3f11ee799a6b410ca3e2249bb3e7c2a5c52c0ad9
Alex <cupokoffi@gmail.com>
2022-05-14 23:42:03 -0400

    Add images

--- README.md
+++ README.md
@@ -1,2 +1,5 @@
-# Sharpen Image
-Image Sharpening code
+# Image Sharpening Code
+## *Compilation Guide*
+ ```
+ To compile this program simply type in the terminal  g++ -g -std=c++11 finalTest.cc  `pkg-config --cflags --libs opencv4`. This works for mac and linux  and we were unable to test it for windows as no one in the group has a windows pc. To run the program type ./a.out and pass the path to the image file you want to sharpen. For example ./a.out input.png. Once running follow the terminal messages to guide you in your image sharpening process. 
+```
--- finalTest.cc
+++ finalTest.cc
@@ -5,8 +5,6 @@
 
 #include <iostream>
 #include <opencv2/opencv.hpp>
-//#include <opencv2/core/core.hpp>
-//#include <opencv2/highgui/highgui.hpp>
 #include <opencv2/imgproc.hpp>
 
 using namespace cv;
@@ -18,43 +16,39 @@ using namespace std;
 
 void unsharpenMask(Mat in);
 
-void laplacianFilter(Mat inputImage) {
-  Mat laplcain;
-  Laplacian(inputImage, laplcain, inputImage.depth(), 3);
- 
-  imshow("laplcian", laplcain);
-  
-
-  convertScaleAbs(laplcain, laplcain);
-  
-  Mat in = inputImage.clone();
-  
+void laplacianFilter(Mat inputImage, double& intensity) {
   Mat laplacian;
-  Laplacian(in, laplacian, in.depth(), 3);
-
+  Laplacian(inputImage, laplacian, inputImage.depth(), 3);
 
   imshow("laplacian", laplacian);
   convertScaleAbs(laplacian, laplacian);
 
-  GaussianBlur(laplacian, laplacian, Size(3, 3), 1.4, 0);
+  int yesOrNo;
+  cout << " Is the image noisy (type 1 to apply gussian blur to laplacain "
+          "mask, 0 for no)\n";
+  cin >> yesOrNo;
+  if(yesOrNo == 1) {
+    GaussianBlur(laplacian, laplacian, Size(3, 3), 0, 0);
+  }
 
+  laplacian *= intensity;
+  imshow("laplacian", laplacian);
   Mat sharp;
-  sharp = in + (-1 * laplacian);
+  sharp = inputImage + (-1 * laplacian);
   imshow("sharp", sharp);
-  imwrite("laplcianSharp.png", sharp);
+  imwrite("laplacianSharp.png", sharp);
+  imwrite("laplacianMask.png", laplacian);
   waitKey(0);
-
 }
 void unsharpenMask(Mat in, double &intensity) {
   Mat input = in.clone();
   
   Mat blurred;
 
-  cv::GaussianBlur(input, blurred, cv::Size(3, 3), 0, 0, BORDER_DEFAULT);
+  GaussianBlur(input, blurred, cv::Size(3, 3), 0, 0, BORDER_DEFAULT);
 
-  cv::imshow("blurred.jpeg", blurred);
+  imshow("blurred.jpeg", blurred);
   
-
   Mat unsharpMask;
   cv::subtract(input, blurred, unsharpMask);
   unsharpMask = unsharpMask * intensity;
@@ -65,49 +59,36 @@ void unsharpenMask(Mat in, double &intensity) {
 
   imshow("sharp.jpeg", sharp);
   waitKey(0);
-  destroyWindow("sharp.jpeg");
+  
+  imwrite("unsharpenMask.png", unsharpMask);
+  imwrite("unsharpenMask-sharpened.png", sharp);
   return;
 }
 
 void Roberts(Mat input, double &intensity) {
   imshow("input", input);
-  Mat RGB[3];
-  split(input, RGB);
+  
   Mat Roberts_x = (Mat_<double>(2, 2) << -1, 0, 0, 1);
   Mat Roberts_y = (Mat_<double>(2, 2) << 0, -1, 1, 0);
 
-  Mat G_xRGB[3];
+  Mat G_x;
  
+  filter2D(input, G_x, input.depth(), Roberts_x);
+
+  Mat G_y;
   
-  filter2D(RGB[0], G_xRGB[0], input.depth(), Roberts_x);
-  filter2D(RGB[1], G_xRGB[1], input.depth(), Roberts_x);
-  filter2D(RGB[2], G_xRGB[2], input.depth(), Roberts_x);
+  filter2D(input, G_y, input.depth(), Roberts_y);
 
-  Mat G_yRGB[3];
-  
+  convertScaleAbs(G_x, G_x);
+  convertScaleAbs(G_y, G_y);
 
-  filter2D(RGB[0], G_yRGB[0], input.depth(), Roberts_y);
-  filter2D(RGB[1], G_yRGB[1], input.depth(), Roberts_y);
-  filter2D(RGB[2], G_yRGB[2], input.depth(), Roberts_y);
+  G_x.convertTo(G_x, CV_32F);
+  G_y.convertTo(G_y, CV_32F);
 
-  Mat G_xTotal;
-  Mat G_yTotal;
-  merge(G_xRGB, 3, G_xTotal);
-  merge(G_yRGB, 3, G_yTotal);
-
-  convertScaleAbs(G_xTotal, G_xTotal);
-  convertScaleAbs(G_yTotal, G_yTotal);
-
-  G_xTotal.convertTo(G_xTotal, CV_32F);
-  G_yTotal.convertTo(G_yTotal, CV_32F);
-
-  
   Mat G_xy;
 
+  magnitude(G_x, G_y, G_xy);
   
-  magnitude(G_xTotal, G_yTotal, G_xy);
-  
-
   convertScaleAbs(G_xy, G_xy);
   imshow("mag", G_xy);
     G_xy *= intensity;
@@ -116,6 +97,7 @@ void Roberts(Mat input, double &intensity) {
 
     imshow("output", output);
     imwrite("RobertsSharpen.png", output);
+    imwrite("RobertsMask.png", G_xy);
 
     waitKey(0);
 }
@@ -123,10 +105,8 @@ void Roberts(Mat input, double &intensity) {
 void sobel(Mat input, double &intensity) {
   imshow("in", input);
   
-  
   Mat Sobel_x = (Mat_<double>(3, 3) << -1, -2, -1, 0, 0, 0, 1, 2, 1);
   Mat Sobel_y = (Mat_<double>(3, 3) << -1,0,1,-2,0,2,-1,0,1);
-  
 
   Mat G_x;
   filter2D(input, G_x, input.depth(), Sobel_x);
@@ -136,13 +116,11 @@ void sobel(Mat input, double &intensity) {
   filter2D(input, G_y, input.depth(), Sobel_y);
   imshow("G_y", G_y);
 
-
   G_x.convertTo(G_x, CV_32F);
   G_y.convertTo(G_y, CV_32F);
   Mat G_xy;
   magnitude(G_x, G_y, G_xy);
   convertScaleAbs(G_xy, G_xy);
-  
 
   G_xy *= intensity;
   imshow("mag", G_xy);
@@ -151,21 +129,19 @@ void sobel(Mat input, double &intensity) {
   add(input, G_xy, output);
   imshow("output", output);
   imwrite("Sobel.png", output);
+  imwrite("SobelMask.png", G_xy);
 
-   waitKey(0);
+  waitKey(0);
 }
 
 
 
 void perwitt(Mat input, double &intensity) {
-  
   imshow("in", input);
-
   
   Mat Sobel_x = (Mat_<double>(3, 3) << -1, -1, -1, 0, 0, 0, 1, 1, 1);
   Mat Sobel_y = (Mat_<double>(3, 3) << -1, 0, 1, -1, 0, 1, -1, 0, 1);
  
-
   Mat G_x;
   filter2D(input, G_x, input.depth(), Sobel_x);
   imshow("G_x", G_x);
@@ -187,7 +163,8 @@ void perwitt(Mat input, double &intensity) {
   add(input, G_xy, output);
 
   imshow("output", output);
-  imwrite("PerwtitSharpen.png", output);
+  imwrite("PrewittSharpen.png", output);
+  imwrite("PrewittMask.png", G_xy);
 
   waitKey(0);
 }
@@ -205,52 +182,40 @@ void equalizeHistogramGRAY( Mat input) {
 }
 
 void equalizeHistogramCOLOR( Mat input) {
-  Mat hist_equalized_image;
-  cvtColor(input, hist_equalized_image, COLOR_BGR2YCrCb);
+  Mat equalizedImage;
+  cvtColor(input, equalizedImage, COLOR_BGR2YCrCb);
 
-  vector<Mat> vec_channels;
-  split(hist_equalized_image, vec_channels);
+  vector<Mat> channels;
+  split(equalizedImage, channels);
 
-  
-  equalizeHist(vec_channels[0], vec_channels[0]);
+  equalizeHist(channels[0], channels[0]);
 
-  
-  merge(vec_channels, hist_equalized_image);
+  merge(channels, equalizedImage);
 
-  
-  cvtColor(hist_equalized_image, hist_equalized_image, COLOR_YCrCb2BGR);
+  cvtColor(equalizedImage, equalizedImage, COLOR_YCrCb2BGR);
 
-  
-  String windowNameOfOriginalImage = "Original Image";
-  String windowNameOfHistogramEqualized = "Histogram Equalized Color Image";
-
-  
-  namedWindow(windowNameOfOriginalImage, WINDOW_NORMAL);
-  namedWindow(windowNameOfHistogramEqualized, WINDOW_NORMAL);
-
-  
-  imshow(windowNameOfOriginalImage, input);
-  imshow(windowNameOfHistogramEqualized, hist_equalized_image);
-  imwrite("ColorEquilized.png", hist_equalized_image);
+  imshow("equalized", equalizedImage);
+  imwrite("ColorEqualized.png", equalizedImage);
 
   waitKey(0);  
 }
 
-int main() {
+int main(int argc, char** argv) {
   int input;
   Mat inputImage;
   String originalImage = "Original";
-  inputImage = imread("ColorEquilized.png", IMREAD_COLOR);
-
-  cout << "Choose a image processing technique: \n\n";
-  cout << "1: show origial image\n";
+  inputImage = imread(argv[1], IMREAD_COLOR);
+  string a, b;
+  cout << "Choose an image processing technique: \n\n";
+  cout << "1: show original image\n";
   cout << "2: Sharpen Image Using laplace filter\n";
   cout << "3: Sharpen using unsharpen mask\n";
   cout << "4: Roberts\n";
   cout << "5: Sobel\n";
   cout << "6: Perwitt\n";
-  cout << "7: Equilize Historam for gray image\n";
-  cout << "8: Equailize histgram for colored image\n";
+  cout << "7: Equalize histogram for gray image\n";
+  cout << "8: Equalize histogram for colored image\n";
+  
 
   double intensity;
   int yesOrNo;
@@ -264,81 +229,77 @@ int main() {
       destroyWindow(originalImage);
       break;
     case 2:
-
-      laplacianFilter(inputImage);
+      cout << "please specify the intesity of the sharpeing.\nUse 1 as default before increasing it\n";
+      cin >> intensity;
+     
+      laplacianFilter(inputImage,intensity);
       break;
     case 3:
 
      
-      cout << "please specify the intesity of the sharpeing\n";
+      cout << "please specify the intensity of the sharpening\n";
       cin >> intensity;
       unsharpenMask(inputImage, intensity);
 
-      cout << "Chnage intensity?\n";
+      cout << "Change intensity?\n Use value 1 as yes and value 0 as no";
       cin >> yesOrNo;
       while(yesOrNo == 1) {
         cout << "please state new intensity\n";
         cin >> intensity;
         unsharpenMask(inputImage, intensity);
-        cout << "change again?\n";
+        cout << "change again?\nUse value 1 as yes an value 0 as no";
         cin >> yesOrNo;
       }
       break;
 
     case 4:
       
-      cout << "please specify the intesity of the sharpeing\n";
+      cout << "please specify the intesity of the sharpeing\nUse 1 as default before increasing it\n";
       cin >> intensity;
       Roberts(inputImage, intensity);
 
-      cout << "Chnage intensity?\n";
+      cout << "Chanage intensity?\nUse value 1 as yes and value 0 as no";
       cin >> yesOrNo;
       while (yesOrNo == 1) {
         cout << "please state new intensity\n";
         cin >> intensity;
         Roberts(inputImage, intensity);
-        cout << "change again?\n";
+        cout << "change again?\nUse value 1 as yes and value 0 as no";
         cin >> yesOrNo;
       }
       break;
     case 5:
 
-      cout << "please specify the intesity of the sharpeing\n";
+      cout << "please specify the intesity of the sharpeing\nUse 1 as default before increasing it\n";
       cin >> intensity;
       sobel(inputImage, intensity);
 
-      cout << "Chnage intensity?\n";
+      cout << "Chanage intensity?\nUse value 1 as yes and value 0 as no";
       cin >> yesOrNo;
       while (yesOrNo == 1) {
         cout << "please state new intensity\n";
         cin >> intensity;
         sobel(inputImage, intensity);
-        cout << "change again?\n";
+        cout << "change again?\nUse value 1 as yes and value 0 as no";
         cin >> yesOrNo;
       }
 
       break;
     case 6:
-      cout << "please specify the intesity of the sharpeing\n";
+      cout << "please specify the intesity of the sharpeing\nUse 1 as default "
+              "before increasing it\n";
       cin >> intensity;
       perwitt(inputImage, intensity);
 
-      cout << "Chnage intensity?\n";
+      cout << "Chnage intensity?\nUse value 1 as yes and value 0 as no";
       cin >> yesOrNo;
       while (yesOrNo == 1) {
         cout << "please state new intensity\n";
         cin >> intensity;
         perwitt(inputImage, intensity);
-        cout << "change again?\n";
+        cout << "change again?\nUse value 1 as yes and value 0 as no";
         cin >> yesOrNo;
       }
       break;
-
-      case 7:
-        equalizeHistogramGRAY(inputImage);
-        break;
-      case 8:
-        equalizeHistogramCOLOR(inputImage);
-        break;
   }
 }
